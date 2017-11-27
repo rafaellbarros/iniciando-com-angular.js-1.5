7fa5875784a8b87b559f170cf3a7c2f92e345587
Rafael Barros <rafaelbarros.df@gmail.com>
2017-11-26 23:18:43 -0200

    Create object person

--- src/app/hello.js
+++ src/app/hello.js
@@ -3,6 +3,16 @@ angular
   .component('app', {
     templateUrl: 'app/hello.html',
     controller: function () {
-      this.hello = 'Hello World!';
+      var vm = this;
+
+      vm.hello = 'Hello World!';
+      vm.person = {
+        name : 'Rafael',
+        lastname: 'Silva'
+      };
+
+      vm.alertMe = function() {
+        alert(vm.person.name + ' ' + vm.person.lastname);
+      };
     }
   });
